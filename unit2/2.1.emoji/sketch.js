a92fa885fb7f36026dea6b9e6ee91a21fc70c1de
ryeeUMW <ryee@mail.umw.edu>
2024-09-21 20:03:27 -0400

    emoji assignment

--- unit2/2.1.emoji/sketch.js
+++ unit2/2.1.emoji/sketch.js
@@ -9,6 +9,33 @@ function setup() {
 function draw() {
   background(220);
 
+  stroke("gold"); //stroke color
+  strokeWeight(8); //stroke weight
+  fill("yellow"); //fill
+
+  ellipse(200,200,250,250); //creating base face
+
+
+  stroke("brown");//stroke color
+  strokeWeight(4); //stroke weight
+  fill("brown"); //fill
+
+  ellipse(150,250,30,60); //left eye
+
+  stroke("brown");//stroke color
+  strokeWeight(4); //stroke weight
+  fill("brown"); //fill
+
+  ellipse(250,250,30,60); //right eye
+
+//drawing the mouth
+
+  noFill();
+
+  arc(200, 210, 160,160,radians(210),radians(330));
+  stroke("brown");
+
+
   rectMode(CENTER);
  
 }
