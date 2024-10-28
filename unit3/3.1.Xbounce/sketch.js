df31f994af5f620954567c0ba08a0ef61ee74d66
ryeeUMW <ryee@mail.umw.edu>
2024-10-28 13:19:16 -0400

    img

---
+++ unit3/3.1.Xbounce/sketch.js
@@ -0,0 +1,69 @@
+let bouncers = [];
+
+class Bouncer {
+  constructor(){
+    this.x=200;
+    this.y=200;
+    this.xspeed=2
+    this.yspeed=3;
+  }
+  update(){
+  background(170,150,160,100);
+  // fill(random(10,250),random(10,250),random(10,250),255);
+  // strokeWeight(random(1,10));
+  circle(this.x, this.y, 50);
+
+  // check for collision with left and right edge
+  if (this.x < 25 | this.x > 375 ){ //changed the collition to be at the edge of the circle rather than the center of the circle
+      this.xspeed = this.xspeed * -1; // reverse the x direction
+      fill(random(10,250),random(10,250),random(10,250),255);//changing color when it hits the edge
+      strokeWeight(random(1,10));
+  }
+  if (this.y < 25 | this.y > 375){
+      this.yspeed = this.yspeed * -1; // reverse the y direction
+      fill(random(10,250),random(10,250),random(10,250),255);//changing color when it hits the edge
+      strokeWeight(random(1,10));
+  }
+
+  this.x = this.x + this.xspeed; // iterate x
+  this.y = this.y + this.yspeed; // iterate y
+  }
+}
+
+function setup() {
+  createCanvas(400, 400);
+
+  for (let b = 0; b < 20; b++){
+    bouncers.push(new Bouncer());
+  }
+}
+
+
+function draw() {
+  for (let r = 0; r < bouncers.length; r++){
+    bouncers[r].update();
+  }
+  // background(170,150,160,100);
+
+  // circle(x, y, 50);
+
+  // // check for collision with left and right edge
+  // if (x < 25 | x > 375 ){ //changed the collition to be at the edge of the circle rather than the center of the circle
+  //     xspeed = xspeed * -1; // reverse the x direction
+  //     fill(random(10,250),random(10,250),random(10,250),255);//changing color when it hits the edge
+  //     strokeWeight(random(1,10));
+  // }
+  // if (y < 25 | y > 375){
+  //     yspeed = yspeed * -1; // reverse the y direction
+  //     fill(random(10,250),random(10,250),random(10,250),255);//changing color when it hits the edge
+  //     strokeWeight(random(1,10));
+  // }
+
+  // x = x + xspeed; // iterate x
+  // y = y + yspeed; // iterate y
+  }
+  function mousePressed(){//making the circle change color and stroke weight when the mouse is pressed
+    bouncers[r].update();
+    fill(random(10,250),random(10,250),random(10,250),255);
+    strokeWeight(random(1,10));
+  }
--- unit3/3.2.follower/sketch.js
+++ unit3/3.2.follower/sketch.js
@@ -1,7 +1,34 @@
+function preload(){
+  // load the image from a file
+  img = loadImage('/Users/a2190/Documents/GitHub/creative-coding/unit3/3.2.follower/ratfollower.gif');
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
+
+let speedfactor = 3;
+let xspeed = speedfactor;
+let yspeed = speedfactor;
+let x = 0; 
+let y = 0;
 }
 
 function draw() {
   background(220);
+
+if (mouseX > x){
+   xspeed = speedfactor;
 }
+else{
+   xspeed = -speedfactor;
+}
+
+if (mouseY > y){
+   yspeed = speedfactor;
+}
+
+else{
+   yspeed = -speedfactor;
+}
+
+ image(img,xspeed - 10,yspeed - 10,10,10);}
