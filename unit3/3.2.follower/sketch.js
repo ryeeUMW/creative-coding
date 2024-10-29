4418bdbf24650e6702a40e5a6f5419d8517e907e
ryeeUMW <ryee@mail.umw.edu>
2024-10-28 22:46:12 -0400

    follower

--- unit3/3.2.follower/sketch.js
+++ unit3/3.2.follower/sketch.js
@@ -1,20 +1,32 @@
-function preload(){
-  // load the image from a file
-  img = loadImage('/Users/a2190/Documents/GitHub/creative-coding/unit3/3.2.follower/ratfollower.gif');
-}
+// function preload(){
+//   // load the image from a file
+//   img = createImage('/Users/a2190/Documents/GitHub/creative-coding/unit3/3.2.follower/ratfollower.gif');
+// }
+
+let debug = false;
+let speedfactor = 3;
+let xspeed = speedfactor;
+let yspeed = speedfactor;
+let x=0; 
+let y=0;
+let d=0;
 
 function setup() {
   createCanvas(800, 800);
 
-let speedfactor = 3;
-let xspeed = speedfactor;
-let yspeed = speedfactor;
-let x = 0; 
-let y = 0;
+x = random(width);
+y = random(height);
 }
 
 function draw() {
   background(220);
+
+d = sqrt((x - mouseX)**2 + (y - mouseY)**2);
+
+x += xspeed;
+y += yspeed;
+
+circle(x,y,50);
 
 if (mouseX > x){
    xspeed = speedfactor;
@@ -31,4 +43,20 @@ else{
    yspeed = -speedfactor;
 }
 
- image(img,xspeed - 10,yspeed - 10,10,10);}
+if (d < 25){
+   fill(random(10,250),random(10,250),random(10,250),255);
+   x = random(width);
+   y = random(height);
+  }
+
+if (debug){
+   textSize(30);
+   text("mouseX:" + mouseX, 50,50);
+   text("mouseY:" + mouseY, 50,80);
+   text("x: " + x, 50,120);
+   text("y: " + y, 50,150);
+   text("d: " + d, 50,180);
+   }
+}
+
+ // image(img,xspeed,yspeed,10,10);}
--- unit3/3.3.ripples/sketch.js
+++ unit3/3.3.ripples/sketch.js
@@ -1,7 +1,26 @@
+let ripplers = [];
+
 function setup() {
   createCanvas(800, 800);
+
+}
+  class Rippler{
+  constructor(x,y){
+    this.x = x,
+    this.y = y,
+    this.diameter = 0
+  }
 }
 
 function draw() {
   background(220);
+
+  for (let i = 0; i < ripplers.length; i++){
+    ripplers[i].draw();
+  }
+}
+
+function mousePressed(){
+draw(circle(mouseX,mouseY,10));
+log.console;
 }
