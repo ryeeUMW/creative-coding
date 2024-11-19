a77e3c517d43a7f6e78c573ef600717182c2940d
ryeeUMW <ryee@mail.umw.edu>
2024-11-19 17:26:46 -0500

    book?

--- unit4/4.3.love-markov/sketch.js
+++ unit4/4.3.love-markov/sketch.js
@@ -1,7 +1,6 @@
-
-
-let rm = RiTa.markov(2);
-
+let rm = RiTa.markov(2); //initialize the Markov model
+let data = document.getElementById("source").innerText;
+rm.addText(data);
 
 
 function preload() {
@@ -9,6 +8,20 @@ function preload() {
 }
 
 function setup(){
-
+	let lines = rm.generate(6);
 	
+	// find the element to add to
+	let letter = select("#letter"); 
+
+	// add a salutation
+	letter.child( createP('My Dearest Harrington,'));
+
+	// loop through the array of lines, adding each as a new p element
+	for (let l = 0; l < lines.length; l++){
+		let paragraph = createP(lines[l]);
+	letter.child(paragraph);
+	}
+
+// add a closing
+letter.child( createP('Sincerely,<br>Markov'));
 }
